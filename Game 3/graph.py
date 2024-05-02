72ef4194ca434bc04b573270abf4458654faa18c
LucasHasting <110124874+LucasHasting@users.noreply.github.com>
2024-05-01 23:38:29 -0500

    Update graph.py

--- Game 3/graph.py
+++ Game 3/graph.py
@@ -1,4 +1,32 @@
 import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+def show_graph(graph):
+    print("Entering stage 1")
+    pos = nx.spring_layout(graph, k=0.3*1/np.sqrt(len(graph.nodes())), iterations=20)
+    print("Entering stage 2")
+    plt.figure(3, figsize=(30, 30))
+    print("Entering stage 3")
+    nx.draw(graph, pos=pos)
+    print("Entering stage 4")
+    nx.draw_networkx_labels(graph, pos=pos)
+    print("Entering stage 5")
+    plt.savefig("graph.png")
+    print("Finished")
+    return
+
+def generate_graph_visual():
+    f = open("all_matrix_nodes.txt", "r")
+    graph = nx.DiGraph()
+    for i in f:
+        matrix_1 = i[:33]
+        matrix_2 = i[34:67]
+        path = i[68]
+        graph.add_edge(matrix_1, matrix_2)
+
+    show_graph(graph)
+    return
 
 def check_all_configurations():
     flag = True
@@ -56,6 +84,7 @@ def get_solution(config):
         matrix_2 = i[34:67]
         path = i[68]
         graph.add_edge(matrix_1, matrix_2)
+        
         if matrix_1 in graph_dict.keys():
             graph_dict[matrix_1][0].append(matrix_2)
             graph_dict[matrix_1][1].append(path)
@@ -70,13 +99,5 @@ def get_solution(config):
     return order
 
 #check_all_configurations() file exists in folder, no need to run again
-print(get_solution("[[1, 0, 2], [0, 1, 0], [1, 0, 0]]"))
-
-
-
-
-
-
-
-
-
+#print(get_solution("[[1, 0, 2], [0, 1, 0], [1, 0, 0]]"))
+generate_graph_visual()
